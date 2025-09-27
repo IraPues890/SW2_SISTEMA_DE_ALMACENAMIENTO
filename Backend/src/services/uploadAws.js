905d73bb08c553dbe6dfbe0ab7d7d8097f46edd0
Leo <leonardo.gonz41@gmail.com>
2025-09-26 18:27:35 -0600

    Añadido comentarios a los servicios del backend y estructurado mejor

--- Backend/src/services/uploadAws.js
+++ Backend/src/services/uploadAws.js
@@ -1,7 +1,12 @@
 const fs = require("fs");
 const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 
+//Definimos la región de S3
+
 const s3 = new S3Client({ region: "us-east-1" });
+
+
+//Definimos el nombre del bucket
 const bucketName = "giomar-nos-debe-broster";
 
 async function uploadToAws(filePath, fileName) {
