79f3d6e12a994ad88d177bab277be120be992ce1
Leo <leonardo.gonz41@gmail.com>
2025-09-27 17:30:22 -0500

    (añadido index.js del repository de AWS y modificacion de la ruta para utilizar el repository

--- Backend/src/routes/awsRoutes.js
+++ Backend/src/routes/awsRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const multer = require("multer");
-const { uploadToAws } = require("../services/uploadAws");
+const awsRepository = require("../services/AWS/awsRepository");
 
 const router = express.Router();
 const upload = multer({ dest: "uploads/" });
@@ -8,7 +8,7 @@ const upload = multer({ dest: "uploads/" });
 // POST /aws/upload
 router.post("/upload", upload.single("file"), async (req, res) => {
   try {
-    await uploadToAws(req.file.path, req.file.originalname);
+    await awsRepository.upload(req.file.path, req.file.originalname);
     res.json({ success: true, message: `Archivo ${req.file.originalname} subido a AWS S3` });
   } catch (err) {
     console.error(err);
