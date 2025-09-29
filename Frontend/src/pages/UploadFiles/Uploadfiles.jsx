844f79f6c6f66e56574ab7a6054d6c14f7ced326
Steel64 <updown1.jp@gmail.com>
2025-09-29 17:15:07 -0500

    organizacion ventanas frontend

---
+++ Frontend/src/pages/UploadFiles/Uploadfiles.jsx
@@ -0,0 +1,12 @@
+
+import './Uploadfiles.css';
+
+function Uploadfiles() {
+  return (
+    <div className="Upload_files">
+        <h1>Upload files</h1>
+    </div>
+  );
+}
+
+export default Uploadfiles;
