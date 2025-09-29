f475422952774fad8a36d0c3673278d0f6997882
Leo <leonardo.gonz41@gmail.com>
2025-09-29 16:41:09 -0600

    Corregido index.js de rutas

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -1,16 +1,8 @@
 const express = require("express");
-const oracleRoutes = require("./oracleRoutes");
-const awsRoutes = require("./awsRoutes");
-const azureRoutes = require("./azureRoutes");
-const gcpRoutes = require("./gcpRoutes");
+const providerRoutes = require("./providerRoutes")
 
 const router = express.Router();
 
-// Agrupamos rutas bajo prefijos
-router.use("/oracle", oracleRoutes);
-router.use("/aws", awsRoutes);
-router.use("/azure", azureRoutes);
-router.use("/gcp", gcpRoutes);
-
+router.use("/storage", providerRoutes);
 module.exports = router;
 
