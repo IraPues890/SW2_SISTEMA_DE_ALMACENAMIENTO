1c47fd39dbb5b30d024c1e063e01aab13a21bf88
20211869-hue <20211869@aloe.ulima.edu.pe>
2025-10-07 22:23:26 -0500

    implementando metodo para eliminar objeto
    Yadiras

--- Backend/src/services/IStorageRepository.js
+++ Backend/src/services/IStorageRepository.js
@@ -6,9 +6,13 @@ class IStorageRepository {
   async listObjects() {
     throw new Error("Se debe implemetar el método listObjects()");
   }
-  
+
   async downloadObject(fileName, destinationPath){
     throw new Error("Se debe implemetar el método downloadObject()");
+  }
+
+  async deleteObject(fileName){
+    throw new Error("Se debe implemetar el método deleteObject()");
   }
 }
 
