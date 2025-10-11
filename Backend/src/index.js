1ef2652c43e2323a83b7dce373f75af2a907d5bf
Dylann Ernesto Rojas Huaytalla <20212362@aloe.ulima.edu.pe>
2025-10-11 13:32:30 -0500

    Implementacion CORS
    Dylann

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,9 +1,16 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 const routes = require("./routes");
 
 const app = express();
+
+// Configurar CORS
+app.use(cors({
+  origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
+  credentials: true
+}));
 
 // Middleware global (JSON, logs, etc.)
 app.use(express.json());
