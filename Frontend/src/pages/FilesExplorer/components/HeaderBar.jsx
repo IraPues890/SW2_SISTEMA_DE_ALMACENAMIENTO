8f7f147f089309cd0cee97fa9362422854266631
leshyG <leonardo.gonz41@gmail.com>
2025-10-24 12:20:13 -0500

    Modificado Fileexplorer.jsx para usar los hooks y editado componentes hardcodeados

--- Frontend/src/pages/FilesExplorer/components/HeaderBar.jsx
+++ Frontend/src/pages/FilesExplorer/components/HeaderBar.jsx
@@ -2,25 +2,19 @@ import React from 'react';
 import { Metrics } from './Metrics';
 import { ViewSwitch } from './ViewComponents';
 
+// AHORA: Solo recibe las props que SÍ utiliza.
 export default function HeaderBar({
   filesCount,
   breadcrumbString,
   activeView,
   setActiveView,
-  query,
-  setQuery,
-  sortOption,
-  setSortOption,
-  setPage,
-  onCreateFolder,
-  onUploadFile,
-  onDownloadSelected,
-  onDeleteSelected,
-  navigate,
 }) {
   return (
     <>
+      {/* Metrics usa filesCount y breadcrumbString */}
       <Metrics totalFiles={filesCount} usedSpace={'1.2 GB'} breadcrumb={breadcrumbString} />
+      
+      {/* ViewSwitch usa activeView y setActiveView */}
       <ViewSwitch activeView={activeView} onChange={setActiveView} />
     </>
   );
