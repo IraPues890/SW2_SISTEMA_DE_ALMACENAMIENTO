046937cefab013f2ceee2b0f0be3061c50f3086d
leshyG <106412608+leshyG@users.noreply.github.com>
2025-11-15 09:27:03 -0500

    Dependencia path en GCP

--- Backend/src/services/GCP/gcpClient.js
+++ Backend/src/services/GCP/gcpClient.js
@@ -1,4 +1,5 @@
 const { Storage } = require("@google-cloud/storage");
+const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 
 class GoogleClient {
