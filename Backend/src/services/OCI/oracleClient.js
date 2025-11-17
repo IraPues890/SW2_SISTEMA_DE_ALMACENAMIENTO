09859d393ce7e6eac003928be1599d85cbb58bc7
leshyG <106412608+leshyG@users.noreply.github.com>
2025-11-17 12:26:32 -0500

    Se añadió una librería necesaria para la autenticación, antes solo lograba listar objetos, ahora es posible la subida

--- Backend/src/services/OCI/oracleClient.js
+++ Backend/src/services/OCI/oracleClient.js
@@ -1,33 +1,39 @@
 const OCI = require("oci-objectstorage");
+const common = require("oci-common");
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 
 class OracleClient {
   constructor() {
     try {
-      const config = {
-        tenancy: process.env.OCI_TENANCY_ID,
-        user: process.env.OCI_USER_ID,
-        fingerprint: process.env.OCI_FINGERPRINT,
-        privateKey: process.env.OCI_PRIVATE_KEY_PATH,
-        region: process.env.OCI_REGION
-      };
+      const provider = new common.SimpleAuthenticationDetailsProvider(
+        process.env.OCI_TENANCY_ID,
+        process.env.OCI_USER_ID,
+        process.env.OCI_FINGERPRINT,
+        process.env.OCI_PRIVATE_KEY_PATH,
+        null,
+        common.Region.fromRegionId(process.env.OCI_REGION)
+      );
+
       this.bucketName = process.env.OCI_BUCKET_NAME;
       this.namespace = process.env.OCI_BUCKET_NAMESPACE;
 
-      if (!this.bucketName || !this.namespace ) {
+      if (!this.bucketName || !this.namespace) {
         throw new Error("Variables de entorno OCI_BUCKET_NAME o OCI_BUCKET_NAMESPACE no encontradas");
       }
-      this.client = new OCI.ObjectStorageClient(config);
-      this.client.endpoint = `https://objectstorage.${config.region}.oraclecloud.com`;
-      
+
+      this.client = new OCI.ObjectStorageClient({
+        authenticationDetailsProvider: provider
+      });
+
       console.log('CLIENTE ORACLE INICIALIZADO CON ÉXITO!!');
 
     } catch (error) {
-      console.warn('CLIENTE ORACLE FALLÓ: ', error.message);
+      console.warn('!!! CLIENTE ORACLE FALLÓ: ', error.message);
       // Fallback para desarrollo local
       this.client = null;
       this.bucketName = null;
+      this.namespace = null;
     }
   }
 
