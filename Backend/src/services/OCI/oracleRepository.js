4a4209e0d5516b0321769480d1a5cffbef898ed6
leshyG <106412608+leshyG@users.noreply.github.com>
2025-11-16 21:10:14 -0500

    Métodos añadidos y endpoit al oracleClient para obtener namespace del bucket. Editato método listObjects de Oracle para devolver un array del mismo tipo que en AWS

--- Backend/src/services/OCI/oracleRepository.js
+++ Backend/src/services/OCI/oracleRepository.js
@@ -5,117 +5,121 @@ const IStorageRepository = require("../IStorageRepository");
 
 class OracleRepository extends IStorageRepository {
   constructor() {
-      super();
-      this.client = oracleClient.getClient();
-      this.bucketName = "mi_bucket";
-      // Alternativa: this.bucketName = process.env.ORACLE_BUCKET
+    super();
+    this.client = oracleClient.getClient();
+    this.bucketName = oracleClient.getBucketName();
+    this.namespace = oracleClient.getNamespace();
   }
 
   async upload(filePath, fileName) {
-      const { value: namespaceName } = await this.client.getNamespace({});
-      const putObjectRequest = {
-        namespaceName,
-        bucketName: this.bucketName,
-        objectName: fileName,
-        putObjectBody: fs.createReadStream(filePath),
+    const putObjectRequest = {
+      namespaceName: this.namespace,
+      bucketName: this.bucketName,
+      objectName: fileName,
+      putObjectBody: fs.createReadStream(filePath),
     };
 
-      await this.client.putObject(putObjectRequest);
-      return {
-        fileName,
-        bucket: this.bucketName,
-        uploaded: true,
-      };
+    await this.client.putObject(putObjectRequest);
+
+    return {
+      fileName,
+      bucket: this.bucketName,
+      uploaded: true,
+    };
   }
 
-  async listObjects() {
-      const { value: namespaceName } = await this.client.getNamespace({});
-      const listObjectsRequest = {
-        namespaceName,
-        bucketName: this.bucketName,
-      };
+  async listObjects(provider) {
+    const listObjectsRequest = {
+      namespaceName: this.namespace,
+      bucketName: this.bucketName,
+      fields: "size,timeCreated",
+    };
+    console.log(listObjectsRequest);
 
-      const response = await this.client.listObjects(listObjectsRequest);
-      const objects = response.listObjects.objects.map(obj => ({
-        fileName: obj.name,
-        size: obj.size,
-        etag: obj.etag,
-        storageTier: obj.storageTier,
-      }));
+    this.client.endpoint = `https://objectstorage.${process.env.OCI_REGION}.oraclecloud.com`;
+    console.log("Endpoint:", this.client.endpoint);
+    const response = await this.client.listObjects(listObjectsRequest);
+    console.log(response.listObjects)
+;    const objects = response.listObjects.objects.map(obj => ({
+      fileName: obj.name,
+      size: obj.size,
+      lastModified: obj.timeCreated,
+      cloud: provider
+    }));
 
-      return {
-        bucket: this.bucketName,
-        objects,
-      };
+    return {
+      bucket: this.bucketName,
+      objects,
+    };
   }
 
   async downloadObject(fileName, destinationPath) {
-      const { value: namespaceName } = await this.client.getNamespace({});
+    const { value: namespaceName } = await this.client.getNamespace({});
 
-      const getObjectRequest = {
-        namespaceName,
-        bucketName: this.bucketName,
-        objectName: fileName,
-      };
+    const getObjectRequest = {
+      namespaceName,
+      bucketName: this.bucketName,
+      objectName: fileName,
+    };
 
-      const dir = path.dirname(destinationPath);
-      await fs.promises.mkdir(dir, { recursive: true });
-      
-      const response = await this.client.getObject(getObjectRequest);
+    const dir = path.dirname(destinationPath);
+    await fs.promises.mkdir(dir, { recursive: true });
 
-      return new Promise((resolve, reject) => {
-        const writeStream = fs.createWriteStream(destinationPath);
-        response.value
-          .pipe(writeStream)
-          .on("finish", () => {
-            resolve({
-              fileName,
-              bucket: this.bucketName,
-              destination: destinationPath,
-              downloaded: true,
-            });
-          })
-          .on("error", reject);
-      });
+    const response = await this.client.getObject(getObjectRequest);
+
+    return new Promise((resolve, reject) => {
+      const writeStream = fs.createWriteStream(destinationPath);
+      response.value
+        .pipe(writeStream)
+        .on("finish", () => {
+          resolve({
+            fileName,
+            bucket: this.bucketName,
+            destination: destinationPath,
+            downloaded: true,
+          });
+        })
+        .on("error", reject);
+    });
   }
 
   async deleteObject(fileName) {
-      const { value: namespaceName } = await this.client.getNamespace({});
+    const { value: namespaceName } = await this.client.getNamespace({});
 
-      const deleteObjectRequest = {
-        namespaceName,
-        bucketName: this.bucketName,
-        objectName: fileName,
-      };
+    const deleteObjectRequest = {
+      namespaceName,
+      bucketName: this.bucketName,
+      objectName: fileName,
+    };
 
-      await this.client.deleteObject(deleteObjectRequest);
+    await this.client.deleteObject(deleteObjectRequest);
 
-      return {
-        fileName,
-        bucket: this.bucketName,
-        deleted: true,
-      };
+    return {
+      fileName,
+      bucket: this.bucketName,
+      deleted: true,
+    };
   }
-  
+
   async createFolder(folderName) {
-      const { value: namespaceName } = await this.client.getNamespace({});
+    const { value: namespaceName } = await this.client.getNamespace({});
 
-      const folderKey = folderName.endsWith("/") ? folderName : `${folderName}/`;
-      const putObjectRequest = {
-        namespaceName,
-        bucketName: this.bucketName,
-        objectName: folderKey,
-        putObjectBody: Buffer.from(""),
-      };
+    const folderKey = folderName.endsWith("/") ? folderName : `${folderName}/`;
+    const putObjectRequest = {
+      namespaceName,
+      bucketName: this.bucketName,
+      objectName: folderKey,
+      putObjectBody: Buffer.from(""),
+    };
 
-      await this.client.putObject(putObjectRequest);
+    await this.client.putObject(putObjectRequest);
 
-      return {
-        folderName: folderKey,
-        bucket: this.bucketName,
-        created: true,
-      };
-    }
+    return {
+      folderName: folderKey,
+      bucket: this.bucketName,
+      created: true,
+    };
+  }
 }
 
 module.exports = OracleRepository;
